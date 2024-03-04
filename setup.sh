9650f5b995e048ef0a3b0a4e207bab9523d253bd
Rahul Tewari <rahultewari75@gmail.com>
2024-03-04 07:46:05 +0000

    add makefile makeopts

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 cat >>~/.profile - <<EOF
 
 # rahul's custom commands
+
+# vim keybindings
 alias vi=vim
 set -o vi
+
+# autocompletions
 source /usr/share/bash-completion/completions/git
+
+# makefile parallelism
+export MAKEOPTS=-j8
 EOF
 
